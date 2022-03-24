3466bc6a5dbea359398a69a1b7e7b606dab69cfe
Gutemberg <gutembergdantas16@hotmail.com>
2022-03-24 18:04:36 -0300

    Adicionado express e nodemon

--- app.js
+++ app.js
@@ -1,4 +1,21 @@
 
-const { principal } = require('./src/index')
+// const { principal } = require('./src/index')
+// principal();
 
-principal();
+const express = require('express');
+const res = require('express/lib/response');
+const port = 8080;
+const app = express();
+
+app.get('/home', function(req, res){
+  res.send('Página Inicial')
+});
+
+app.get('/quem-somos', (request, response) => {
+  response.send('Esta é a página quem somos')
+});
+
+// Função de callback
+app.listen(port, function(){
+  console.log('Servidor funcionando na porta ' + port);
+});
